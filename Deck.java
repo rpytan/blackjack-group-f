af5281811c405e28260058ad230dfdc2288341ef
SeredithM <seredithm@gmail.com>
2016-12-07 11:09:08 -0600

    Added shuffle() and restart()
    Shuffle shuffles the deck, restart puts all cards in the discard pile back into the deck, then shuffles the deck.

--- Deck.java
+++ Deck.java
@@ -31,4 +31,19 @@ public class Deck {
 		this.cards.remove(0);
 	}
 	
+	/**
+	 * Shuffles all cards not in the discard pile.
+	 */
+	public void shuffle() {
+		Collections.shuffle(this.cards, new Random());
+	}
+	
+	/**
+	 * Puts all cards in the discard pile back into the deck, then shuffles the deck.
+	 */
+	public void restart() {
+		this.cards.addAll(this.discard);
+		this.discard.clear();
+		shuffle();
+	}
 }
