9a95637ebab3b29cc44974d5517d160748af33bc
rpytan <rpytan@gmail.com>
2016-12-07 10:49:01 -0600

    Update Hand.java

--- Hand.java
+++ Hand.java
@@ -3,13 +3,14 @@ public class Hand { //constructor
 	Card hand = new Card("","",1); //compilation from 
 	int totalPV = 0;
 	
-	Hand() {
+	Hand() 
+	{
 		for(int x=0;x<hand.length;x++)
-			totalPV+=hand.getPointValue();
+			totalPV+=hand.getValue();
 	}	
 	
-	public Card toString() { //displays hand
-		
+	public Card toString() //displays hand	
+	{ 
 		for(int x=0;x<hand.length;x++)
 			return hand;
 	}
@@ -18,5 +19,4 @@ public class Hand { //constructor
 	{
 		return totalPV;
 	}
-
 }
